ad5a4179786a9c900590bb8332520752ea66ce62
Jakub Filip <comodore@seznam.cz>
2023-01-24 19:28:35 +0100

    Initial commit

---
+++ data.py
@@ -0,0 +1,31 @@
+# jason from open Trivia DB
+question_data = [
+    {"category": "Entertainment: Board Games", "type": "boolean", "difficulty": "easy",
+     "question": "Snakes and Ladders was originally created in India?", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "History", "type": "boolean", "difficulty": "easy",
+                                       "question": "The Tiananmen Square protests of 1989 were held in Hong Kong.",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Matt Damon played an astronaut stranded on an extraterrestrial planet in "
+                 "both of the movies Interstellar and The Martian.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Linus Torvalds created Linux and Git.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Nutella is produced by the German company Ferrero.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Sports", "type": "boolean", "difficulty": "medium",
+                                      "question": "Tennis was once known as Racquetball.", "correct_answer": "False",
+                                      "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "The French word to travel is &quot;Travail&quot;", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Celebrities", "type": "boolean", "difficulty": "easy",
+                                      "question": "Marilyn Monroe was born on July 1, 1926.", "correct_answer": "False",
+                                      "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+     "question": "In career mode of &quot;Need for Speed: Underground 2&quot;, "
+                 "the first car the player can drive is the BMW M3 GTR.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Cartoon & Animations", "type": "boolean", "difficulty": "medium",
+     "question": "Nutcracker Suite was one of the musical pieces featured in Disney&#039;s 1940&#039;s film Fantasia.",
+     "correct_answer": "True", "incorrect_answers": ["False"]}
+]
---
+++ question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    # Creating a question class with attributes write down bellow
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+
